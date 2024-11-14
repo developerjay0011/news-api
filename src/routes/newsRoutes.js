76ad0d09a3889da62f724839f9d53142b6bc4af7
Jay Dhimar <jaydhimar1@gmail.com>
2024-11-14 17:47:29 +0530

    fix issue

--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../config/multerConfig');
 const authenticateToken = require('../middleware/authMiddleware');
-const { createNews, updateNews, deleteNews, setNewsStatus ,getAllNews, getNonExpiredNews} = require('../controllers/newsController');
+const { createNews, updateNews, deleteNews, setNewsStatus, getAllNews, getNonExpiredNews } = require('../controllers/newsController');
 
 router.post('/create', authenticateToken, upload.single('image'), createNews);
 router.put('/update', authenticateToken, upload.single('image'), updateNews);
 router.delete('/delete/:id', authenticateToken, deleteNews);
 router.put('/set-status', authenticateToken, setNewsStatus);
 router.get('/get-all', getAllNews);
-router.get('/public/non-expired', getNonExpiredNews);
+router.get('/non-expired', getNonExpiredNews);
 
 module.exports = router;
